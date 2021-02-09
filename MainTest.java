c0d4d8497570c9396ab127f18d61aa03d2624701
anandhang <45062262+anandhang@users.noreply.github.com>
2021-02-09 11:05:43 +0530

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -3,7 +3,5 @@ public class MainTest {
     public static void main(String[] arg){
         System.out.println("-------Jenkins TEST----------");
         System.out.println("-------Jenkins TEST Adde line----------");
-        System.out.println("-------Jenkins TEST Adde line----------");
-        System.out.println("-------Jenkins TEST Adde line----------");
-    }
+            }
 }
