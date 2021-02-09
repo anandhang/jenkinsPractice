eccf0195e4d75aeab062b20c213a979d86502e20
anandhang <45062262+anandhang@users.noreply.github.com>
2021-02-09 09:59:51 +0530

    Jenkins practice test code

---
+++ Grand_Father_Family.java
@@ -0,0 +1,10 @@
+public class Grand_Father_Family {
+    String grandFatherName = "Periyathambi";
+    String grandMotherName = "Kaveri";
+
+    public void display_grandFather(){
+        System.out.println("-:Family Members:-");
+        System.out.println("Grand Father Name: "+ grandFatherName);
+        System.out.println("Grand Mother Name: "+ grandMotherName);
+    }
+}
