50adb9969bc9fb523d8d2d40d729ba25d3a7509b
anandhang <45062262+anandhang@users.noreply.github.com>
2021-02-09 10:39:41 +0530

    Delete Son_Family.java

--- Son_Family.java
+++
@@ -1,9 +0,0 @@
-public class Son_Family extends Father_Family{
-    String myName = "Anandhan";
-    String wifeName = "SubhaShree";
-
-    public void display_Son(){
-        System.out.println("My Name: "+myName);
-        System.out.println("My Wife Name: "+wifeName);
-    }
-}
