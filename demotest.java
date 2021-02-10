0423f1a26127133079642f0a24434f20d33a71be
anandhang <45062262+anandhang@users.noreply.github.com>
2021-02-10 11:19:17 +0530

    Update demotest.java

--- demotest.java
+++ demotest.java
@@ -2,5 +2,6 @@ public class demotest {
     public static void main(String[] arg){
         System.out.println("My test demo project");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
